61a20ef7d642c9baa540745c31a74ee129758a67
Marek Lörincz <lorinczdev@gmail.com>
2022-12-16 18:45:32 +0100

    Config update

--- config/lunar-reviews.php
+++ config/lunar-reviews.php
@@ -5,4 +5,27 @@
  */
 return [
 
+    // Prefix for all the API routes
+    // Leave empty if you don't want to use a prefix
+    'route_prefix' => 'api',
+
+    // Middleware for all the API routes
+    'route_middleware' => ['api'],
+
+    // Configuration for specific domains
+    'domains' => [
+        'reviews' => [
+            'model' => Dystcz\LunarReviews\Domain\Reviews\Models\Review::class,
+
+            // Route groups which get registered
+            // If you want to change the behaviour or add some data,
+            // simply extend the package product groups and add your logic
+            'route_groups' => [
+                'reviews' => Dystcz\LunarReviews\Domain\Reviews\Http\Routing\ReviewRouteGroup::class,
+            ],
+
+            // Default pagination
+            'pagination' => 12,
+        ],
+    ],
 ];
