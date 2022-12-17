38a9f57e1582985d63c6426b99ab81b4fea78d38
Marek Lörincz <lorinczdev@gmail.com>
2022-12-17 18:33:25 +0100

    Fix schema drop if exists table name

--- database/migrations/2022_12_16_172316_create_reviews_table.php
+++ database/migrations/2022_12_16_172316_create_reviews_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('lunar_product_reviews');
+        Schema::dropIfExists($this->prefix.'reviews');
     }
 };
