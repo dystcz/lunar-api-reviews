ae4469039cbe5429b95b0cbef8182bffb2fbbd01
Jakub Theimer <jakub@dy.st>
2022-12-22 21:30:43 +0700

    style fix, add docblocks

--- src/Domain/Reviews/Models/Review.php
+++ src/Domain/Reviews/Models/Review.php
@@ -4,6 +4,8 @@ namespace Dystcz\LunarReviews\Domain\Reviews\Models;
 
 use Auth;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Facades\Config;
 use Lunar\Base\BaseModel;
 
@@ -24,23 +26,39 @@ class Review extends BaseModel
 
     protected static function booted()
     {
-        static::creating(static function (Review $review): void {
+        static::creating(static function (self $review): void {
             $review->user_id = $review->user_id ?: Auth::user()?->id;
         });
     }
 
-    public function purchasable(): \Illuminate\Database\Eloquent\Relations\MorphTo
+    /**
+     * Purchasable relation.
+     *
+     * @return MorphTo
+     */
+    public function purchasable(): MorphTo
     {
         return $this->morphTo();
     }
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    /**
+     * User relation.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(
             Config::get('auth.providers.users.model')
         );
     }
 
+    /**
+     * Scope published reviews.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
     public function scopePublished(Builder $query): Builder
     {
         return $query->whereNotNull('published_at');
