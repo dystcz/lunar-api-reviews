9d09c5286883617db02f139c7a766afe192860fd
Jakub Theimer <jakub@dy.st>
2023-04-16 23:55:45 +0200

    cleanup

--- src/Domain/Reviews/JsonApi/V1/ReviewSchema.php
+++ src/Domain/Reviews/JsonApi/V1/ReviewSchema.php
@@ -6,39 +6,22 @@ use Dystcz\LunarApi\Domain\JsonApi\Eloquent\Schema;
 use Dystcz\LunarApiReviews\Domain\Reviews\Models\Review;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
-use LaravelJsonApi\Eloquent\Contracts\Paginator;
 use LaravelJsonApi\Eloquent\Fields\DateTime;
 use LaravelJsonApi\Eloquent\Fields\ID;
 use LaravelJsonApi\Eloquent\Fields\Number;
 use LaravelJsonApi\Eloquent\Fields\Relations\BelongsTo;
 use LaravelJsonApi\Eloquent\Fields\Relations\MorphTo;
 use LaravelJsonApi\Eloquent\Fields\Str;
-use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
-use LaravelJsonApi\Eloquent\Pagination\PagePagination;
 
 class ReviewSchema extends Schema
 {
     /**
-     * The default paging parameters to use if the client supplies none.
-     *
-     * @var array|null
-     */
-    protected ?array $defaultPagination = ['number' => 1];
-
-    /**
      * The model the schema corresponds to.
-     *
-     * @var string
      */
     public static string $model = Review::class;
 
     /**
      * Build an index query for this resource.
-     *
-     * @param  Request|null  $request
-     * @param  Builder  $query
-     * @return Builder
      */
     public function indexQuery(?Request $request, Builder $query): Builder
     {
@@ -46,69 +29,38 @@ class ReviewSchema extends Schema
     }
 
     /**
-     * Get the include paths supported by this resource.
-     *
-     * @return string[]|iterable
-     */
-    public function includePaths(): iterable
-    {
-        return [
-
-        ];
-    }
-
-    /**
      * Get the resource fields.
      *
      * @return array
      */
-    public function fields(): array
+    public function fields(): iterable
     {
         return [
             ID::make(),
 
             Str::make('comment'),
             Number::make('rating')->sortable(),
-
             Number::make('purchasable_id'),
             Str::make('purchasable_type'),
+            DateTime::make('published_at')
+                ->serializeUsing(
+                    static fn ($value) => $value->format('Y-m-d H:i:s'),
+                )
+                ->sortable(),
 
-            DateTime::make('published_at')->sortable(),
+            BelongsTo::make('user')
+                ->serializeUsing(
+                    static fn ($relation) => $relation->withoutLinks(),
+                ),
+            MorphTo::make('purchasable', 'reviews')
+                ->types('products', 'variants'),
 
-            BelongsTo::make('user'),
-            MorphTo::make('purchasable', 'reviews'),
+            ...parent::fields(),
         ];
-    }
-
-    /**
-     * Get the resource filters.
-     *
-     * @return array
-     */
-    public function filters(): array
-    {
-        return [
-            WhereIdIn::make($this),
-        ];
-    }
-
-    /**
-     * Get the resource paginator.
-     *
-     * @return Paginator|null
-     */
-    public function pagination(): ?Paginator
-    {
-        return PagePagination::make()
-            ->withDefaultPerPage(
-                Config::get('lunar-reviews.domains.reviews.pagination', 12)
-            );
     }
 
     /**
      * Get the JSON:API resource type.
-     *
-     * @return string
      */
     public static function type(): string
     {
