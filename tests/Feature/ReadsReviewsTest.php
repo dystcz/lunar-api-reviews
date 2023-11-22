bd0103db0b6b058e1410a25010018aa9c28f71f2
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-22 20:51:45 +0000

    fix tests

--- tests/Feature/ReadsReviewsTest.php
+++ tests/Feature/ReadsReviewsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Dystcz\LunarApi\Domain\ProductVariants\Models\ProductVariant;
 use Dystcz\LunarApiReviews\Domain\Reviews\Models\Review;
 use Dystcz\LunarApiReviews\Tests\TestCase;
@@ -8,10 +9,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(TestCase::class, RefreshDatabase::class);
 
 it('can list reviews', function () {
+    /** @var TestCase $this */
     $reviews = Review::factory()
         ->for(ProductVariant::factory(), 'purchasable')
         ->count(5)
-        ->create();
+        ->create(['published_at' => Carbon::now()]);
 
     $self = 'http://localhost/api/v1/reviews';
 
@@ -24,6 +26,7 @@ it('can list reviews', function () {
 });
 
 it('can show a single review', function () {
+    /** @var TestCase $this */
     /** @var Review $review */
     $review = Review::factory()
         ->for(ProductVariant::factory(), 'purchasable')
