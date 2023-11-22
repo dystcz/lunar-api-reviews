1741f0b909323790d632be5e67f5f4d367d63ce8
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-22 21:12:35 +0000

    use lunar api product variants controller

--- src/Domain/Reviews/Http/Routing/ReviewRouteGroup.php
+++ src/Domain/Reviews/Http/Routing/ReviewRouteGroup.php
@@ -4,9 +4,9 @@ namespace Dystcz\LunarApiReviews\Domain\Reviews\Http\Routing;
 
 use Dystcz\LunarApi\Domain\Products\Http\Controllers\ProductsController;
 use Dystcz\LunarApi\Domain\Products\JsonApi\V1\ProductSchema;
+use Dystcz\LunarApi\Domain\ProductVariants\Http\Controllers\ProductVariantsController;
 use Dystcz\LunarApi\Domain\ProductVariants\JsonApi\V1\ProductVariantSchema;
 use Dystcz\LunarApi\Routing\RouteGroup;
-use Dystcz\LunarApiReviews\Domain\ProductVariants\Http\Controllers\ProductVariantsController;
 use Dystcz\LunarApiReviews\Domain\Reviews\Http\Controllers\PublishReviewsController;
 use Dystcz\LunarApiReviews\Domain\Reviews\Http\Controllers\ReviewsController;
 use Dystcz\LunarApiReviews\Domain\Reviews\JsonApi\V1\ReviewSchema;
@@ -26,21 +26,25 @@ class ReviewRouteGroup extends RouteGroup
         JsonApiRoute::server('v1')
             ->prefix('v1')
             ->resources(function (ResourceRegistrar $server) {
-                $server->resource(ReviewSchema::type(), ReviewsController::class)
+                $server
+                    ->resource(ReviewSchema::type(), ReviewsController::class)
                     ->except('update');
 
-                $server->resource(ReviewSchema::type(), PublishReviewsController::class)
+                $server
+                    ->resource(ReviewSchema::type(), PublishReviewsController::class)
                     ->actions('-actions', function ($actions) {
                         $actions->withId()->post('publish');
                         $actions->withId()->delete('unpublish');
                     })->only();
 
-                $server->resource(ProductSchema::type(), ProductsController::class)
+                $server
+                    ->resource(ProductSchema::type(), ProductsController::class)
                     ->relationships(function (Relationships $relationships) {
                         $relationships->hasMany('reviews')->only('index');
                     })->only();
 
-                $server->resource(ProductVariantSchema::type(), ProductVariantsController::class)
+                $server
+                    ->resource(ProductVariantSchema::type(), ProductVariantsController::class)
                     ->relationships(function ($relationships) {
                         $relationships->hasMany('reviews')->only('index');
                     })->only();
