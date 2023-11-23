d15e3951ab02e1e851e7703d3ba20697e1224c36
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-23 14:04:51 +0000

    include auth user unpublished reviews when configured

--- src/Domain/Reviews/Builders/ReviewBuilder.php
+++ src/Domain/Reviews/Builders/ReviewBuilder.php
@@ -4,6 +4,8 @@ namespace Dystcz\LunarApiReviews\Domain\Reviews\Builders;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 /**
  * @method self published()
@@ -16,7 +18,21 @@ class ReviewBuilder extends Builder
     public function published(): self
     {
         return $this
-            ->where('published_at', '!=', null)
-            ->where('published_at', '<=', Carbon::now());
+            ->where(function (Builder $query) {
+                $query
+                    ->where('published_at', '!=', null)
+                    ->where('published_at', '<=', Carbon::now());
+            })
+            ->orWhere(function (Builder $query) {
+                $query
+                    ->when(
+                        Auth::check() && Config::get(
+                            'lunar-api.reviews.domains.reviews.settings.include_unpublished_auth_user_reviews',
+                            false,
+                        ),
+                        fn (Builder $query) => $query->where('user_id', Auth::id()),
+                        fn (Builder $query) => $query,
+                    );
+            });
     }
 }
