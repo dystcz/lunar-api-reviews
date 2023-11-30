1a4c2edc8d288d5c1f45e7dd77ef2e7da018f28a
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-30 01:14:44 +0000

    add review rating attribute

--- src/Domain/Reviews/Traits/InteractsWithReviews.php
+++ src/Domain/Reviews/Traits/InteractsWithReviews.php
@@ -2,7 +2,9 @@
 
 namespace Dystcz\LunarApiReviews\Domain\Reviews\Traits;
 
+use Dystcz\LunarApiReviews\Domain\Reviews\Builders\ReviewBuilder;
 use Dystcz\LunarApiReviews\Domain\Reviews\Models\Review;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
@@ -15,5 +17,27 @@ trait InteractsWithReviews
     {
         /** @var Model $this */
         return $this->morphMany(Review::class, 'purchasable');
+    }
+
+    /**
+     * Return average rating.
+     */
+    public function reviewRating(): Attribute
+    {
+        return Attribute::make(
+            get: function (): ?float {
+                /** @var Model $this */
+                if ($this->relationLoaded('reviews')) {
+                    return $this->reviews?->avg('rating');
+                }
+
+                /** @var ReviewBuilder $reviewBuilder */
+                $reviewBuilder = $this->reviews();
+
+                ray($reviewBuilder);
+
+                return $reviewBuilder->avg('rating');
+            }
+        );
     }
 }
